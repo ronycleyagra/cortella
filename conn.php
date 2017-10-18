63c14910ab70b7f79c3fa425974e3dfd210d99fe
Ronycley <ronycley@gmail.com>
2017-10-18 16:19:53 -0200

    Arquivo de conexão
    Arquivo de conexão

--- conn.php
+++
@@ -1,9 +0,0 @@
-<?php
-$host = "10.16.4.39"; 
-$usuario = "dx3_homologacao";
-$senha = "452136987";
-$banco = "siacnet";
- 
-$conn = mssql_connect($host, $usuario, $senha);
-$db = mssql_select_db($banco, $conn);
-?>
